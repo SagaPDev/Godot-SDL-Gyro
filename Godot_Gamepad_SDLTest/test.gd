6df54f12de52d5afb34dc96c47aee4f827236e99
Gabriel Salazar <GSalazar.go@gmail.com>
2024-02-27 13:36:08 -0400

    fix unexpected rotations by pasing the the controller orientation data as a quaternion

--- Godot_Gamepad_SDLTest/test.gd
+++ Godot_Gamepad_SDLTest/test.gd
@@ -10,11 +10,12 @@ func _ready():
 func _process(delta):
   #Gyro.gamepadPoling()
   orientation=Gyro.gamepadPoling()
-
-  $Sketchfab_Scene.quaternion.w=orientation[0]
-  $Sketchfab_Scene.quaternion.z=orientation[1]
-  $Sketchfab_Scene.quaternion.y=orientation[2]
-  $Sketchfab_Scene.quaternion.x=-orientation[3]
+  
+  $Sketchfab_Scene.quaternion=Quaternion(orientation[3],orientation[2],-orientation[1],orientation[0])
+  #$Sketchfab_Scene.quaternion.w=orientation[0]
+  #$Sketchfab_Scene.quaternion.z=orientation[1]
+  #$Sketchfab_Scene.quaternion.y=orientation[2]
+  #$Sketchfab_Scene.quaternion.x=-orientation[3]
   #$MeshInstance3D.quaternion.x=orientation[0]
   #$MeshInstance3D.quaternion.y=orientation[1]
   #$MeshInstance3D.quaternion.z=orientation[2]
